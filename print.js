c66f2093730040443d9d74c8ab358bbc38089bf1
MarcelaJS <124182469+MarcelaJS@users.noreply.github.com>
2023-03-06 23:30:13 -0300

    Add files via upload

--- print.js
+++ print.js
@@ -3,3 +3,6 @@ const btn=document.querySelector(".print");
                 window.print();
             }
             btn.addEventListener("click",printORsave);
+            /*Este codigo lo saque de talkerscode.com,
+            la idea era poner algo de javascript, aunque
+            no se me ocurria como incluirlo en un resume o cv */
